e9b27e02d9adf8d828bab132a8d612b742ccc485
cement <32463636@qq.com>
2017-10-28 01:09:18 +0800

    merge dev branch,add local storage

--- js/background.js
+++ js/background.js
@@ -1,45 +1,50 @@
+let mainMenuId = chrome.contextMenus.create({
+    title: 'YSH右键翻译：%s', // %s表示选中的文字
+    contexts: ['selection']
+})
 chrome.contextMenus.create({
-    title: '英译汉：%s', // %s表示选中的文字
+    title: '英->汉', // %s表示选中的文字
     contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+    parentId:mainMenuId,
     onclick: function(onClickData, tab) {
         let tranText = onClickData.selectionText;
         let tabId = tab.id;
-        sendHttpRequest(tranText, 'en','zh',function(resp) {
-            //alert(resp);
-            chrome.tabs.sendMessage(tabId, resp, function(response) {
-                if (callback) {
-                    callback(response);
-                }
-            });
+        translate(tranText, 'en','zh',function(resp) {
+            chrome.tabs.sendMessage(tabId, resp);
         });
     }
 });
 chrome.contextMenus.create({
-    title: '汉泽英：%s', // %s表示选中的文字
+    title: '汉->英', // %s表示选中的文字
     contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+    parentId:mainMenuId,
     onclick: function(onClickData, tab) {
         let tranText = onClickData.selectionText;
         let tabId = tab.id;
-        sendHttpRequest(tranText, 'zh','en',function(resp) {
-            //alert(resp);
-            chrome.tabs.sendMessage(tabId, resp, function(response) {
-                if (callback) {
-                    callback(response);
-                }
-            });
+        translate(tranText, 'zh','en',function(resp) {
+            chrome.tabs.sendMessage(tabId, resp);
         });
     }
 });
+function translate(transText,fromLang,toLang,callback) {
+    let from = fromLang||'en';
+    let to = toLang||'zh';
+    let result = localStorage.getItem(transText+'['+from+'-'+to+']');
+    if(result){
+        let resp = JSON.parse(result);
+        callback(resp);
+    }else{
+        sendHttpRequest(transText,from,to,callback);
+    }
+}
 
-function sendHttpRequest(transText,fromLang,toLang,callback) {
+function sendHttpRequest(transText,from,to,callback) {
 
     let appid = '20171025000090729';
     let key = 'RkJM2wwl4Vos_6ZVtgUu';
     let salt = (new Date).getTime();
     let query = transText;
     // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
-    let from = fromLang||'en';
-    let to = toLang||'zh';
     
     let str1 = appid + query + salt + key;
     let sign = MD5(str1);
@@ -50,14 +55,16 @@ function sendHttpRequest(transText,fromLang,toLang,callback) {
     xhr.open("GET", encodeUrl, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            //if(xhr.status===200){
+            if(xhr.status=200){
                 // JSON解析器不会执行攻击者设计的脚本.
-                var resp = JSON.parse(xhr.responseText);
+                let resp = JSON.parse(xhr.responseText).trans_result[0];
                 callback(resp);
-            // }else(){
-            //     var error = xhr.status+':'+xhr.statusText;
-            //     callback(error);
-            // }
+                let key = query+'['+from+'-'+to+']';
+                localStorage.setItem(key,JSON.stringify(resp));
+             }else{
+                let error = xhr.status+':'+xhr.statusText;
+                callback(error);
+            }
         }
     }
     xhr.send();
